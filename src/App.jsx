095b775ccc7cd716defe0f7f812f5862c3724f33
Rohit Jadhav <rohitbjadhav03@gmail.com>
2025-10-28 17:45:58 +0530

    fix: correct VITE_API_URL usage

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseTable from "./components/ExpenseTable";
 import ExpenseChart from "./components/ExpenseChart";
 
-const API_URL = "import.meta.env.VITE_API_URL;"; // replace with Render URL after deploy
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
   const [expenses, setExpenses] = useState([]);
